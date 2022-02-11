d6678d52decfccbb7f1d8f7c2da72d8fdfefa669
Rmapati <brahmanboy005@gmail.com>
2022-02-11 14:47:41 +0530

    commit

--- Linked_List/Linked_list_basics/Linked List Quetions/Reverse Linked List.cpp
+++ Linked_List/Linked_list_basics/Linked List Quetions/Reverse Linked List.cpp
@@ -8,6 +8,8 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ 
+ //Iterative method
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
@@ -29,5 +31,54 @@ public:
         return prev;
         
         
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//recursive method
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+void reverse(ListNode* &head,ListNode* curr,ListNode*prev){
+    //base case
+    if(curr==NULL){
+        head=prev;
+        return;
+    }
+    ListNode* forward=curr->next;
+    reverse(head,forward,curr);
+    curr->next=prev;
+}
+class Solution{
+public:
+    ListNode* reverseList(ListNode* head) {
+      ListNode*curr=head;
+      ListNode* prev=NULL;
+      reverse(head,curr,prev);
+      return head;
+       
     }
 };
